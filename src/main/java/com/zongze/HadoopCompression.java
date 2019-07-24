dea7def3ea812962729348706bf7e6fbe3370449
xiezongze <xiezz@yitkeji.com>
2019-07-24 12:03:42 +0800

    a

--- src/main/java/com/zongze/HadoopCompression.java
+++ src/main/java/com/zongze/HadoopCompression.java
@@ -1,13 +1,16 @@
 package com.zongze;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IOUtils;
 import org.apache.hadoop.io.compress.*;
 import org.apache.hadoop.util.ReflectionUtils;
+
 import java.io.*;
+
+import com.hadoop.compression.lzo.LzoCodec;
 
 /**
  * Create By xzz on 2019/7/23
- * <p>
  * hadoop压缩相关
  */
 public class HadoopCompression {
@@ -15,13 +18,13 @@ public class HadoopCompression {
 
     public static void main(String[] args) throws IOException {
 
-        Class[] classes = {DefaultCodec.class, GzipCodec.class, BZip2Codec.class, SnappyCodec.class,Lz4Codec.class};
+        Class[] classes = {DefaultCodec.class, GzipCodec.class, BZip2Codec.class, LzoCodec.class};
         //文件从外部传入
         String sourcePath = "C:\\Users\\Administrator\\Desktop\\soft-gz\\aa.doc";
         Configuration configuration = new Configuration();
         for (Class clazz : classes) {
             compress(clazz, configuration, sourcePath);
-            unCompress(clazz,configuration,sourcePath);
+            unCompress(clazz, configuration, sourcePath);
         }
     }
 
@@ -35,7 +38,7 @@ public class HadoopCompression {
             //获取压缩后缀名称
             String extName = compress.getDefaultExtension();
             CompressionInputStream in = compress.createInputStream(new FileInputStream(sourcePath + extName), decompressor);
-            IOUtils.copyBytes(in,new FileOutputStream(sourcePath+"un"+extName),1024);
+            IOUtils.copyBytes(in, new FileOutputStream(sourcePath + "un" + extName), 1024);
             in.close();
         } catch (IOException e) {
             e.printStackTrace();
